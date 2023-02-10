71a8d95d8d1a14a70902dc68375b802fb665c060
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-02-10 20:53:23 +0900

    Coin data & Data Types

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -85,7 +85,7 @@ const Coins = () => {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>
+              <Link to={`/${coin.id}`} state={coin}>
                 <Img
                   src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
                 />
