68e9b2ab1650582a30949e9287c55e944b58d060
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-02-13 23:20:10 +0900

    각 코인 내용 및 Price Chart tab

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coin from "./Coin";
 import Coins from "./Coins";
-
+import Chart from "./Chart";
+import Price from "./Price";
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Coins />} />
-        <Route path="/:coinId" element={<Coin />} />
+        <Route path="/:coinId" element={<Coin />}>
+          <Route path="price" element={<Price />}></Route>
+          <Route path="chart" element={<Chart />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
